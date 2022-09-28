4c6f6de5bd808c22c44b7015e533b2b1ef5e65fe
Jonny G <110258278+jonathanlevi14@users.noreply.github.com>
2022-09-28 09:17:58 -0700

    add first prompts

--- script.js
+++ script.js
@@ -12,20 +12,30 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
 
 
-//geting how many characters they want
-var howLong = prompt("Choose a length for your Password and type it here: At Least 8 characters, no more than 128.")
-  if (howLong < 8){
+
+
+
+//button event
+generateBtn.onclick = function() {
+  // //advise them on which button to click
+  var advisor = alert("Choose 'OK' for Yes and 'Cancel' for No. " + "No need to enter text unless asked");
+ // //geting how many characters they want
+  var howLong = prompt("Choose a length for your Password and type it here: At Least 8 characters, no more than 128.")
+  if (howLong < 8 || howLong>129){
     prompt("Choose a length for your Password and type it here: At Least 8 character, no more than 128")
   }else {}
   console.log(howLong)
+  
+  var bigLetter = prompt("Do you want Upper Case letters in your password?");
+}
 
-//advise them on which button to click
-var advisor = alert("Choose 'OK' for Yes and 'Cancel' for No. " + "No need to enter text unless asked");
 
-// var character = prompt("WOuld you like to use UpperCase in your password?")
+
+
+// // var character = prompt("WOuld you like to use UpperCase in your password?")
 //  if (character === true){
 // } else if (character === false){
 
