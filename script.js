304f98c003047b74419edb623898385c1c1f1b44
Jonny G <110258278+jonathanlevi14@users.noreply.github.com>
2022-09-29 00:41:00 -0700

    refactor the code

--- script.js
+++ script.js
@@ -1,69 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var numbers = ["1","2","3","4","5","6","7","8","9","0",]
+var specialChar =["!","@","#","$","%","&"]
+var passwordArray = [""]
+var organizedArray = ""
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword)
+generateBtn.addEventListener("click", generatePassword);
 
-
-
-
-
-//button event
 function generatePassword() {
-  // //advise them on which button to click
-  var advisor = alert("Choose 'OK' for Yes and 'Cancel' for No. " + "No need to enter text unless asked");
- // //geting how many characters they want
-  var howLong = prompt("Choose a length for your Password and type it here: At Least 8 characters, no more than 128.")
-  if (howLong < 8 || howLong>129){
-    prompt("Choose a length for your Password and type it here: At Least 8 character, no more than 128")
-  }else {}
-  
+  alert("Choose 'OK' for Yes and 'Cancel' for No. ");
 
-  //Capital Letters?
+  var howLong = prompt(
+    "Choose a length for your Password and type it here: At Least 8 characters, no more than 128."
+  );
+  if (howLong < 8 || howLong > 129) {
+    prompt(
+      "Choose a length for your Password and type it here: At Least 8 characters, no more than 128."
+    );
+    generatePassword();
+  }
+
   var bigLetter = confirm("Do you want Upper Case letters in your password?");
-    if (bigLetter === true) {
-      //code for adding uppercase letters
-    } else {}
+  if (bigLetter === true) {
+    passwordArray = upperCaseLetters.concat(passwordArray);
+  }
 
-
-  //Lower Case Letters
   var smallLetter = confirm("Do you want Lower Case letters in your password");
-    if (bigLetter === true) {
-    //code for adding uppercase letters
-    } else {}
+  if (smallLetter === true) {
+    passwordArray = lowerCaseLetters.concat(passwordArray);
+  }
 
+  var numeric = confirm("Do you want Numbers in your password?");
+  if (numeric === true) {
+    passwordArray = numbers.concat(passwordArray);
+  }
 
-  //Numbers
-  var numeric = confirm("Do you want Numbers in your password?")
-    if (numeric ===true) {
-      //code for adding numbers
-    } else{}
-
-  //Special Characters
-  var special = confrim("Do you want Special Characters in your password")
-  if (special) ===true) {
-    //code for adding numbers
-  } else{}
-
+  var special = confirm("Do you want Special Characters in your password");
+  if (special === true) {
+    passwordArray = specialChar.concat(passwordArray);
+  }
+  var randomArray = [];
+  for (var i = 0; i <= howLong - 1; i++) {
+    var password = passwordArray[Math.floor(Math.random() * i)];
+    randomArray.push(password);
+  }
+  var organizedArray = randomArray.join("");
+  console.log(organizedArray)
+  return organizedArray;
 }
 
 
-
-
-// let letters = ["a","b","c","d","e","f","g","h","i",""j,"k","l","m","n","o","p","q","r","s","t","u","","","","",""]
-
-// // var character = prompt("WOuld you like to use UpperCase in your password?")
-//  if (character === true){
-// } else if (character === false){
-
-// }
